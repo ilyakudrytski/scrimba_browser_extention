bfb3cb5e1b7c79c726eec8af5bbbaaced3845d6d
Ilya Kudrytski <ilya.kudrytski@gmail.com>
2021-06-29 22:06:55 +0300

    added manifest.json, finished extention

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
 const inputEl = document.getElementById('input-el');
 const saveBtn = document.getElementById('input-btn');
-const ulEl = document.querySelector('#ul-el')
+const delBtn = document.getElementById('delete-btn');
+const tabBtn = document.getElementById('tab-btn');
+const ulEl = document.querySelector('#ul-el');
 let myLeads = [];
-saveBtn.addEventListener('click', saveLead);
 
-function saveLead() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ''
-    renderLeads()
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+
+    render(myLeads)
 }
 
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+
+function render(leads) {
+    let listItems = "";
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
         <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
             </a>
         </li>
     `
     }
-    ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems;
+}
+
+saveBtn.addEventListener('click', saveLead);
+delBtn.addEventListener('click', deleteLead);
+
+tabBtn.addEventListener('click', function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    })
+})
+
+function saveLead() {
+    myLeads.push(inputEl.value);
+    inputEl.value = '';
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    render(myLeads);
+
+
+}
+
+function deleteLead() {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
 }
